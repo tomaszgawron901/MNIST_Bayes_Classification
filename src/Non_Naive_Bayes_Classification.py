0265d09e35f2760a08161ec4b86cb1080a03e915
tomaszgawron901 <tomasz.gawron@microsoft.wsei.edu.pl>
2020-06-30 16:58:37 +0200

    Bayes classification methods separated to different files.

--- src/Non_Naive_Bayes_Classification.py
+++ src/Non_Naive_Bayes_Classification.py
@@ -2,18 +2,19 @@ import numpy as np
 from scipy import stats
 
 
-class NaiveBayesClassification:
+class NonNaiveBayesClassification:
     def __init__(self):
         self.gaussian = []
         self.labels = np.empty(0)
 
     def train(self, X: np.ndarray, y: np.ndarray, smoothing=0.001):
         self.labels = np.unique(y)
+        n, d = X.shape
         for l in self.labels:
             divided_data = X[y == l]
             x_mean = np.mean(divided_data, axis=0)
-            var = np.var(divided_data, axis=0) + smoothing
-            self.gaussian.append({'x_mean': x_mean, 'var': var})
+            cov = np.cov(divided_data.T) + np.eye(d)*smoothing
+            self.gaussian.append({'x_mean': x_mean, 'cov': cov})
 
     def classify(self, X: np.ndarray):
         n, d = np.shape(X)
@@ -21,8 +22,6 @@ class NaiveBayesClassification:
         P = np.empty((n, k), dtype=np.float)
         for i in range(k):
             x_mean = self.gaussian[i]['x_mean']
-            var = self.gaussian[i]['var']
-            P[:, i] = stats.multivariate_normal.logpdf(X, mean=x_mean, cov=var)
+            cov = self.gaussian[i]['cov']
+            P[:, i] = stats.multivariate_normal.logpdf(X, mean=x_mean, cov=cov)
         return np.argmax(P, axis=1)
-
-
